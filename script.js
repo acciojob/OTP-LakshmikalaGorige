755a202d741116a92992d197b4fd18ea329b68b8
LakshmikalaGorige <124250141+LakshmikalaGorige@users.noreply.github.com>
2023-11-24 12:48:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+let inputs = document.querySelectorAll("input");
+let ind = 0;
+for ( let i = 0; i < inputs.length; i++ ){
+    inputs[i].addEventListener("keyup", onKeyup);
+}
+
+inputs[ind].focus();
+
+function onKeyup(event){
+    if(event.key === "Backspace"){
+        if(ind == 0){
+            return;
+        }
+        else{
+            ind--;
+            inputs[ind].focus();
+        }
+    }
+    else{
+        if(ind < 5){
+        ind++;
+        inputs[ind].focus();
+        }
+        else{
+            inputs[ind].blur();
+        }
+    }
+    
+}
